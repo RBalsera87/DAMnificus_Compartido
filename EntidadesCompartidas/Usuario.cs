1627aca41dfad026e5c4252ea2ea9196f69e8181
anton <anton@ANTON-PC-LN>
2019-05-31 17:33:54 +0200

    Añado clase usuarios necesaria para el formulario "Administracion"

--- EntidadesCompartidas/Usuario.cs
+++ EntidadesCompartidas/Usuario.cs
@@ -3,11 +3,104 @@ using System.Collections.Generic;
 
 namespace EntidadesCompartidas
 {
-    class Usuario
+    public class Usuario
     {
-        public string nombre { get; set; }
-        public string pass { get; set; }
-        public string token { get; set; }
-        public Dictionary<int, string> coleccion { get; set; }
+        //public string nombre { get; set; }
+        //public string pass { get; set; }
+        //public string token { get; set; }
+        //public Dictionary<int, string> coleccion { get; set; }
+        private string id;
+        private string user;
+        private string email;
+        private string nombre;
+        private string apellidos;
+        private string credenciales;
+        public Usuario() { }
+        public Usuario(string id, string usuario, string email, string nombre, string apellidos, string credenciales)
+        {
+            this.id = id;
+            this.user = usuario;
+            this.email = email;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.credenciales = credenciales;
+        }
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string User
+        {
+            get
+            {
+                return user;
+            }
+
+            set
+            {
+                user = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellidos
+        {
+            get
+            {
+                return apellidos;
+            }
+
+            set
+            {
+                apellidos = value;
+            }
+        }
+
+        public string Credenciales
+        {
+            get
+            {
+                return credenciales;
+            }
+
+            set
+            {
+                credenciales = value;
+            }
+        }
     }
 }
